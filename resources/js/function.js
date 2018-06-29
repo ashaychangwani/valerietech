52d4e2ac468cc935814f66b7761f1bd326f64b6c
Ashay <ashay@iMac.local>
2018-06-30 00:29:26 +0530

    Home page responsive-Nav bar pending

--- resources/js/function.js
+++ resources/js/function.js
@@ -36,7 +36,7 @@ function showSlides(n) {
     hbuttons[1].style.display="inline-block";
   htext[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
-  timeOut=setTimeout(plusSlides,5000,1);
+  //timeOut=setTimeout(plusSlides,5000,1);
     
     
 }
@@ -48,28 +48,45 @@ var divs2 = $('blockquote[id^="testimonials2-"]').hide(),
     i2 = 0;
 var divs3 = $('blockquote[id^="testimonials3-"]').hide(),
     i3 = 0;
-(function cycle() { 
-    divs1.eq(i1).fadeIn(1000)
-              .delay(3500)
-              .fadeOut(1000).delay(1000);
-    divs2.eq(i2).delay(1000)
-                .fadeIn(1000)
-                .delay(3500)
-                .fadeOut(1000,cycle);
+if( $(window).width() > 768)
+{
+    (function cycle() { 
+        divs1.eq(i1).fadeIn(1000)
+                  .delay(3500)
+                  .fadeOut(1000).delay(1000);
+        divs2.eq(i2).delay(1000)
+                    .fadeIn(1000)
+                    .delay(3500)
+                    .fadeOut(1000,cycle);
 
-    divs3.eq(i3).delay(500)
-                .fadeIn(1000)
-                .delay(3500)
-                .fadeOut(1000);
-    i1 = ++i1 % 2;
-    i2=i1;
-    i3=i1;
-})();
-  
-        
+        divs3.eq(i3).delay(500)
+                    .fadeIn(1000)
+                    .delay(3500)
+                    .fadeOut(1000);
+        i1 = ++i1 % 2;
+        i2=i1;
+        i3=i1;
+    })();
+}
+else{
+    (function cycle() { 
+        divs1.eq(i1).fadeIn(1000)
+                  .delay(3500)
+                  .fadeOut(1000).delay(1000);
+        divs2.eq(i2).delay(500)
+                    .fadeIn(1000)
+                    .delay(3000)
+                    .fadeOut(1000);
+        divs3.eq(i3).delay(1000)
+                    .fadeIn(1000)
+                    .delay(2500)
+                    .fadeOut(1000,cycle);
 
-        
-        
-        
+        i1 = ++i1 % 2;
+        i2=i1;
+        i3=i1;
+    })();
+}
+       
 });
 
