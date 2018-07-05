5885a10148e69102c7b7d3fe60f223eb4a0f7acb
Moksh Jain <mokshjain98@gmail.com>
2018-07-06 02:50:49 +0530

    exclusive-clean-code

--- ProgressBar/app.js
+++ ProgressBar/app.js
@@ -1,6 +1,6 @@
  $(document).ready(function(){
     
-    var width, k, name, email, min, max, priority1=1, priority2=1, priority3=1, game1, game2, game3, software1, software2, software3, otherPriority, otherGame, otherSoftware, buttonPressed;
+    var width, k, name, email, min, max, priority1, priority2, priority3, game1, game2, game3, software1, software2, software3, otherPriority, otherGame, otherSoftware, buttonPressed;
 	
 
     document.querySelector('.p1').style.visibility = '';
@@ -17,40 +17,26 @@
     width = 25;
     k = 1;
      
-     $('.submitButton').click(function(){
-         buttonPressed = $(this).attr('name');
-     });
+	$('.submitButton').click(function(){buttonPressed = $(this).attr('name');});
      
     $(".main-form").submit(function(event){
         event.preventDefault();
         if(buttonPressed === "next"){
-        if(k == 1)
-        {
-            p1();
-        }
+			
+        if(k == 1){p1();}
+		
         document.querySelector('.p' + k).style.display = 'none';
         k = k+1;
         document.querySelector('.p' + k).style.display = 'block';
             
-        if(k > 1)
-        {
-                var test=document.getElementsByName("back")[0].style.display="inline-block";
-
-        }
+        if(k > 1){var test=document.getElementsByName("back")[0].style.display="inline-block";}
         if(k == 3)
         {
             p3();
             document. getElementsByName('next').textContent = 'Submit';
-            
         }
-        if(k == 4)
-        {
-            p4();        
-        }
-        progressBarWidth(k);
-        
-
-            
+        if(k == 4){p4();}
+        progressBarWidth(k);            
         }
         else if(buttonPressed === "back"){
             
@@ -60,31 +46,19 @@
             k = k-1;
             document.querySelector('.p' + k).style.display = 'block';
         }
-        if(k > 1)
-        {        
-            document.getElementsByName("back")[0].style.display="inline-block";
-
-        }
-        else{
-        document.getElementsByName("back")[0].style.display="none";
-            
-        }
+        if(k > 1){document.getElementsByName("back")[0].style.display="inline-block";}
+		
+        else{document.getElementsByName("back")[0].style.display="none";}
+		
         if(k <= 3)
         {
             document.getElementsByName('next')[0].value = 'Next';
             document.getElementsByName('next')[0].style.display = "inline-block";
         }
-        if(k == 1)
-        {
-            p1();
-        }
-        if(k == 3)
-        {
-            p3();
-        }
+        if(k == 1){p1();}
+		
+        if(k == 3){p3();}
         progressBarWidth(k);
-        
-
         }
     });
 
@@ -97,23 +71,15 @@
             max = document.getElementById('max').value;
         };
      
-     document.querySelector('.priority1').addEventListener('change', function(){
-                priority1 = document.querySelector('.priority1').selectedIndex;
-            });
+			document.querySelector('.priority1').addEventListener('change', function(){priority1 = document.querySelector('.priority1').selectedIndex;});
 
-            document.querySelector('.priority2').addEventListener('change', function(){
-                priority2 = document.querySelector('.priority2').selectedIndex;
-                checkPriority(priority2);
-            });
+            document.querySelector('.priority2').addEventListener('change', function(){priority2 = document.querySelector('.priority2').selectedIndex;});
 
-            document.querySelector('.priority3').addEventListener('change', function(){
-                priority3 = document.querySelector('.priority3').selectedIndex;
-                checkPriority(priority3);
-            });
+            document.querySelector('.priority3').addEventListener('change', function(){priority3 = document.querySelector('.priority3').selectedIndex;});
     var p3 = function()
     {
         
-        if(priority1 === 2 || priority2 === 2 || priority3 === 2)
+        if(priority1 === 1 || priority2 === 1 || priority3 === 1)
         {
             document.querySelector(".games1").style.display = "inline-block";
             document.querySelector(".games2").style.display = "inline-block";
@@ -141,22 +107,24 @@
             document.querySelector(".otherGame").style.display = "none";
             document.getElementsByName("gameLabel")[0].style.display = "none";
         }
-        alert("t");
         document.getElementsByName('next')[0].value = 'Submit';
 
     };
-document.querySelector('.softwares1').addEventListener('change', function(){
+			document.querySelector('.softwares1').addEventListener('change', function(){
                 software1 = document.querySelector('.softwares1').selectedIndex;
+				exclusive(software1, 2, 3);
             });
 
             document.querySelector('.softwares2').addEventListener('change', function(){
                 software2 = document.querySelector('.softwares2').selectedIndex;
                 checkSoftware(software2);
+				exclusive(software2, 1, 3);
             });
 
             document.querySelector('.softwares3').addEventListener('change', function(){
                 software3 = document.querySelector('.softwares3').selectedIndex;
                 checkSoftware(software3);
+				exclusive(software3, 1, 2);
             });
     var p4 = function()
     {
@@ -174,7 +142,6 @@ document.querySelector('.softwares1').addEventListener('change', function(){
     var checkGame = function(x){
         if(x === 17)
         {
-            //document.getElementById('otherGame').required = true;
             document.querySelector(".otherGame").style.display = "inline-block";
             document.getElementsByName("gameLabel")[0].style.display = "inline-block";
             otherGame = document.getElementById('otherGame').value;
@@ -186,18 +153,29 @@ document.querySelector('.softwares1').addEventListener('change', function(){
         }
     };
     var checkSoftware = function(x){
-        if(x === 9)
+        if(x === 8)
         {
-            //document.getElementById('otherSoftware').required = true;
             document.getElementById('otherSoftware').style.display = "inline-block";
             document.getElementsByName("softwareLabel")[0].style.display = "inline-block";
             otherSoftware = document.getElementById('otherSoftware').value;
         }
-        if(software2 !== 9 && software3 !== 9)
+        if(software2 !== 8 && software3 !== 8)
         {
             document.getElementsByName("softwareLabel")[0].style.display = "none";
             document.querySelector('.otherSoftware').style.display = "none";
         }
     };
-
+	var exclusive = function(x, y, z){
+		if(x === 1 || x === 2){
+			document.getElementById('win7-' + y).disabled = true;
+			document.getElementById('win10-' + y).disabled = true;
+			document.getElementById('win7-' + z).disabled = true;
+			document.getElementById('win10-' + z).disabled = true;
+		} else{
+			document.getElementById('win7-' + y).disabled = false;
+			document.getElementById('win10-' + y).disabled = false;
+			document.getElementById('win7-' + z).disabled = false;
+			document.getElementById('win10-' + z).disabled = false;
+		}
+	};
 }); 
