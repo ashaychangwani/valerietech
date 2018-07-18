22850286986d3c9c743f94595e07236d7add949f
AshayC <ashaychangwani@gmail.com>
2018-07-19 01:26:21 +0530

    PHP done till price

---
+++ resources/php/form.php
@@ -0,0 +1,22 @@
+<?php
+    $name=$_GET["name"];
+    $name=str_replace(array("\r","\n"),array(" "," "),$name);
+    $email=filter_var(trim($_GET["email"]), FILTER_SANITIZE_EMAIL);
+    $message="Test";
+    $price=$_GET["amount"];
+    $recipient="ashaychangwani@gmail.com";
+    
+    $subject="New contact from $name";
+
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message: \n$message\n";
+
+
+    $email_headers="From: $name <$email>";
+
+//    echo "name: $name\nEmail: $email"
+//    mail($recipient,$subject,$email_content,$email_headers);
+
+//    header("Location: http://www.valerietech.in");
+?>
