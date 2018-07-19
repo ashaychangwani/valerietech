c61f3a3e02acc4fd83bcb8f99610bba49a894f49
AshayC <ashaychangwani@gmail.com>
2018-07-19 21:01:48 +0530

    Form done

--- resources/php/form.php
+++ resources/php/form.php
@@ -1,27 +1,45 @@
 <?php
     $name=$_POST["name"];
-    //$name=str_replace(array("\r","\n"),array(" "," "),$name);
-    //$email=filter_var(trim($_GET["email"]), //FILTER_SANITIZE_EMAIL);
-    $message="Test";
-    //$price=$_GET["amount"];
-    
-    echo "<pre> Name:$name";
-    print_r($_POST);
-    echo "</pre>";
+    $email=$_POST["email"];
+    $amount=$_POST["amount"];
+    $priority1=$_POST["priority1"];
+    $priority2=$_POST["priority2"];
+    $priority3=$_POST["priority3"];
+    $specificReq=$_POST["specificReq"];
+    $softwares1=$_POST["softwares1"];
+    $softwares2=$_POST["softwares2"];
+    $softwares3=$_POST["softwares3"];
+    $otherSoftware=$_POST["otherSoftware"];
+    $games1=$_POST["games1"];
+    $games2=$_POST["games2"];
+    $games3=$_POST["games3"];
+    $otherGame=$_POST["otherGame"];
 
-    $recipient="ashaychangwani@gmail.com";
+
+    $recipient="mokshjain98@gmail.com";
     
     $subject="New contact from $name";
 
     $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Message: \n$message\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Amount: $amount\n";
+    $email_content .= "Priority 1: $priority1\n";
+    $email_content .= "Priority 2: $priority2\n";
+    $email_content .= "Priority 3: $priority3\n";
+    $email_content .= "Specific Requirements: $specificReq\n";
+    $email_content .= "Software 1: $softwares1\n";
+    $email_content .= "Software 2: $softwares2\n";
+    $email_content .= "Software 3: $softwares3\n";
+    $email_content .= "Other Software: $otherSoftware\n";
+    $email_content .= "Games 1: $games1\n";
+    $email_content .= "Games 2: $games2\n";
+    $email_content .= "Games 3: $games3\n";
+    $email_content .= "Other Game: $otherGame\n";
 
 
     $email_headers="From: $name <$email>";
 
-//    echo "name: $name\nEmail: $email"
-//    mail($recipient,$subject,$email_content,$email_headers);
+    mail($recipient,$subject,$email_content,$email_headers);
 
-//    header("Location: http://www.valerietech.in");
+    header("Location: http://www.valerietech.in");
 ?>
