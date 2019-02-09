c3be42048514ee64bb4cce4866335537c4c149cc
Ashay Changwani <ashay@Ashays-iMac.local>
2019-02-09 18:47:29 +0530

    added phone to contact, fixed alignment of contact form for queries

--- resources/php/contact.php
+++ resources/php/contact.php
@@ -1,6 +1,7 @@
 <?php
     $name=$_POST["name"];
     $email=$_POST["email"];
+    $phone=$_POST["phone"];
     $find=$_POST["otherFind"];
 
     $recipient="team@valerietech.in";
@@ -9,6 +10,7 @@
 
     $email_content = "Name: $name\n";
     $email_content .= "Email: $email\n";
+    $email_content .= "Phone: $phone\n";
     $email_content .= "Source: $findUs\n";
     $email_content .= "Other (source): $otherFind\n";
 
