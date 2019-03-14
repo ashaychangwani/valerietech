7bedf5cdc939eadf28155b52427d7c1365219656
Ashay Changwani <ashay@Ashays-iMac.local>
2019-03-15 01:18:40 +0530

    fixed early fade in for index

--- resources/js/function.js
+++ resources/js/function.js
@@ -121,7 +121,7 @@ $(document).ready(function(){
     $('.js--wp-1').waypoint(function(direction){
          $('.js--wp-1').addClass('animated fadeIn');
     },{
-        offset: '50%'
+        offset: '65%'
     })
        
     
